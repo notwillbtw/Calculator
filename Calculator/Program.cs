0374e8f1d9b9d78b297ee161f172467192b0cbed
notwillbtw <167638457+notwillbtw@users.noreply.github.com>
2024-05-12 21:36:00 +0100

    finished ttorial! starting challenges, struggling to think of a name for a class.

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,49 +1,24 @@
 ﻿using System.Text.RegularExpressions;
+using CalculatorLibrary;
 
-class Calculator
-{
-    public static double DoOperation(double num1, double num2, string op)
-    {
-        double result = double.NaN; // Default value is "not-a-number" if an operation, such as division, could result in an error.
-
-        // Use a switch statement to do the math.
-        switch (op)
-        {
-            case "a":
-                result = num1 + num2;
-                break;
-            case "s":
-                result = num1 - num2;
-                break;
-            case "m":
-                result = num1 * num2;
-                break;
-            case "d":
-                // Ask the user to enter a non-zero divisor.
-                if (num2 != 0)
-                {
-                    result = num1 / num2;
-                }
-                break;
-            // Return text for an incorrect option entry.
-            default:
-                break;
-        }
-        return result;
-    }
-}
 
 class Program
 {
     static void Main(string[] args)
     {
+        int timesUsed = 0;
+
         bool endApp = false;
         // Display title as the C# console calculator app.
         Console.WriteLine("Console Calculator in C#\r");
         Console.WriteLine("------------------------\n");
 
+        Calculator calculator = new Calculator();
+
         while (!endApp)
         {
+            timesUsed++;
+
             // Declare variables and set to empty.
             // Use Nullable types (with ?) to match type of System.Console.ReadLine
             string? numInput1 = "";
@@ -91,7 +66,7 @@ class Program
             {
                 try
                 {
-                    result = Calculator.DoOperation(cleanNum1, cleanNum2, op);
+                    result = calculator.DoOperation(timesUsed, cleanNum1, cleanNum2, op);
                     if (double.IsNaN(result))
                     {
                         Console.WriteLine("This operation will result in a mathematical error.\n");
@@ -106,11 +81,14 @@ class Program
             Console.WriteLine("------------------------\n");
 
             // Wait for the user to respond before closing.
-            Console.Write("Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
+            Console.Write($"You have used the calculator {timesUsed} time(s). Press 'n' and Enter to close the app, or press any other key and Enter to continue: ");
             if (Console.ReadLine() == "n") endApp = true;
 
             Console.WriteLine("\n"); // Friendly linespacing.
         }
+
+        // Add call to close the JSON writer before return
+        calculator.Finish();
         return;
     }
 }
